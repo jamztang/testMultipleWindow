a778253e7b4994b0914cf0a4a23619c4bc6eb3e2
James Tang <james@goodnotesapp.com>
2019-12-17 15:12:36 +0800

    Initial Commit

---
+++ testMultipleWindows/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testMultipleWindows
+//
+//  Created by James Tang on 17/12/2019.
+//  Copyright © 2019 James Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
